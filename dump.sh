74c1696ece591cd2197346fbbbfec1b992a5c88d
Andrzej Fiedukowicz <fiedukow@gmail.com>
2020-09-26 19:49:26 +0000

    Add automatic funkwhale upload to dump.sh

--- dump.sh
+++ dump.sh
@@ -1,5 +1,9 @@
 #/bin/bash -e
 
+# Fill those in for automatic upload to funkwhale.
+USER_TOKEN=
+LIBRARY_UID=
+
 # See install_debian.sh to install abcde and its soft deps used below.
 
 # -Q query all data sources in the default order
@@ -10,4 +14,11 @@
 # -V verbose
 # -p pad track numbers with 0
 # -j number of encoder process that can run at once
-abcde -Q musicbrainz,cddb,cdtext -x -o'mp3:-b 160' -G -N -V -p -j 16
+abcde -Q musicbrainz,cddb,cdtext -x -o'mp3:-b 160' -G -N -V -p -j 16 | tee rip.log
+
+OUTPUT_DIR=`cat rip.log | grep "copying cover" | awk 'NF>1{print $NF}'`
+
+echo "Uploading..."
+funkwhale -t $USER_TOKEN uploads create $LIBRARY_UID $OUTPUT_DIR/*.mp3
+
+
