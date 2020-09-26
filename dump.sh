539fd779c031bd82ec35da914855abbfe463df86
Andrzej Fiedukowicz <fiedukow@gmail.com>
2020-09-26 21:57:58 +0000

    Embed covers directly into mp3s

--- dump.sh
+++ dump.sh
@@ -9,12 +9,12 @@ LIBRARY_UID=
 # -Q query all data sources in the default order
 # -x eject cd after it's ripped
 # -o output 160kbps MP3 file
-# -G download cover jpeg
+# -B download cover jpeg and embed it into mp3
 # -N non interactive - answer to all questions with defaults
 # -V verbose
 # -p pad track numbers with 0
 # -j number of encoder process that can run at once
-abcde -Q musicbrainz,cddb,cdtext -x -o'mp3:-b 160' -G -N -V -p -j 16 | tee rip.log
+abcde -Q musicbrainz,cddb,cdtext -x -o'mp3:-b 160' -B -N -V -p -j 16 | tee rip.log
 
 OUTPUT_DIR=`cat rip.log | grep "copying cover" | awk 'NF>1{print $NF}'`
 
--- install_debian.sh
+++ install_debian.sh
@@ -4,3 +4,4 @@ sudo apt-get install cdparanoia
 sudo apt-get install lame
 sudo apt-get install id3v2
 sudo apt-get install abcde
+sudo apt-get install eyed3
